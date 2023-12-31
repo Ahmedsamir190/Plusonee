5a42726883fc0667276022ad69102d7ca063b1c5
Ahmed samir <109394049+Ahmedsamir190@users.noreply.github.com>
2023-12-31 12:52:50 +0200

    Delete src directory

--- src/components/protectroute/ProtectRoute.js
+++
@@ -1,9 +0,0 @@
-import UseAuth from "../useAuth/UseAuth";
-import { Navigate, Outlet } from "react-router-dom";
-
-function ProtectRoute() {
-  const { currentUser } = UseAuth();
-  return currentUser ? <Outlet /> : <Navigate to="/signin" />;
-}
-
-export default ProtectRoute;
